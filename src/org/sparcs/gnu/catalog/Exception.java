ab307b4150356be8664b612856711aa86aeffccc
alphamin <alphamin92@gmail.com>
2014-05-10 20:23:10 +0900

    카탈로그 생성

--- src/org/sparcs/gnu/catalog/Exception.java
+++ src/org/sparcs/gnu/catalog/Exception.java
@@ -1,6 +1,13 @@
 package org.sparcs.gnu.catalog;
-
+/**
+ * 예외 처리할 과목 정보를 갖고 있는다.
+ * @author Alphamin
+ *
+ */
 public class Exception {
+	/**
+	 * 생성자.
+	 */
 	public Exception(){
 		
 	}
--- src/org/sparcs/gnu/catalog/Rule.java
+++ src/org/sparcs/gnu/catalog/Rule.java
@@ -1,6 +1,13 @@
 package org.sparcs.gnu.catalog;
-
+/**
+ * 졸업 요건 처리 정보를 갖고 있는다.
+ * @author Alphamin
+ *
+ */
 public class Rule {
+	/**
+	 * 생성자.
+	 */
 	public Rule(){
 		
 	}
