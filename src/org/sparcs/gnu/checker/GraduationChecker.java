21bd2b1d96ff1a1bb9a7c9d573155674f007efa2
leeopop <dlrmsghd@gmail.com>
2014-05-10 20:19:10 +0900

    더미 생성 완료
    CLOSED - task 5: Checker 더미 생성 
    https://github.com/Graduation-for-Noble-Undergraduates/graduation-condition-checker/issues/issues/issue/5

---
+++ src/org/sparcs/gnu/checker/GraduationChecker.java
@@ -0,0 +1,12 @@
+package org.sparcs.gnu.checker;
+
+import org.sparcs.gnu.catalog.Catalog;
+
+public class GraduationChecker {
+	private Catalog catalog;
+
+	public GraduationChecker(Catalog catalog)
+	{
+		this.catalog = catalog;
+	}
+}
