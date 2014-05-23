19868ebdd263bef65bbefea938ba1c967cd2255c
coffee <poapoap@gmail.com>
2014-05-23 22:31:48 +0900

    cs.conf라는 user input을 parse하여 xml형식으로 간단하게 작성

--- src/org/sparcs/gnu/parser/Parse.java
+++ src/org/sparcs/gnu/parser/Parse.java
@@ -1,6 +1,14 @@
 package org.sparcs.gnu.parser;
 
+import java.io.FileReader;
+
+import org.jdom2.Document;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import org.sparcs.gnu.catalog.Catalog;
+
+import xtc.parser.ParseError;
+import xtc.parser.SemanticValue;
 
 public class Parse {
 
@@ -20,11 +28,37 @@ public class Parse {
 	 * Parse user input to make intermediate representation
 	 * @param input
 	 * @return intermediate representation
+	 * @throws Exception 
 	 */
-	public static String parseRawInput(String input) {
+	public static String parseRawInput(String input) throws Exception {
 		String result = null;
-		//TODO
+		String filename = System.getProperty("user.dir") + "/conf/cs.conf";
+		
+		InputGrammar parse = new InputGrammar(new FileReader(filename), filename);
+		Object o = parse.pSpec(0);
+		System.out.println();
+		if(o instanceof SemanticValue) {
+			SemanticValue res = (SemanticValue) o;
+			Document d = res.semanticValue();
+			XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+			out.output(d, System.out);
+		}
+		else if(o instanceof ParseError) {
+			System.err.println("?");
+		}
+		else {
+			System.out.println("?");
+		}
 		
 		return result;
+	}
+	
+	/** this main method is for testing above functions.
+	 * free to remove this
+	 * @author coffee
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		parseRawInput("");
 	}
 }
