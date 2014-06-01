6a079f121553ea2602dc3c169b2c281150fc25c0
leeopop <dlrmsghd@gmail.com>
2014-06-01 21:40:41 +0900

    DB 구조 살짝 변경

--- database/database.sql
+++ database/database.sql
@@ -6,17 +6,17 @@ DROP TABLE IF EXISTS `metadata` ;
 -- Table `mydb`.`table1`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `grade` (
-  `index` INT NOT NULL,
-  `year` INT NOT NULL,
-  `semester` TEXT NOT NULL,
+  `unique` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `index` INT NULL,
+  `year` INT NULL,
+  `semester` TEXT NULL,
   `number` TEXT NOT NULL,
   `code` TEXT NOT NULL,
   `section` TEXT NULL,
   `type` TEXT NOT NULL,
   `credit` INT NOT NULL,
   `au` INT NOT NULL,
-  `grade` TEXT NOT NULL,
-  PRIMARY KEY (`index`))
+  `grade` TEXT NOT NULL)
 ;
 
 CREATE TABLE IF NOT EXISTS `metadata` (
